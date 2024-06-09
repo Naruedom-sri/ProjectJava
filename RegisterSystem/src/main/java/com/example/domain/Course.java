4b2e25e8b06f83f691d5dcdc9d65512de093f696
Naruedom-sri <142306666+Naruedom-sri@users.noreply.github.com>
2024-06-09 23:47:37 +0700

    Add files via upload

---
+++ RegisterSystem/src/main/java/com/example/domain/Course.java
@@ -0,0 +1,39 @@
+package com.example.domain;
+
+import java.io.Serializable;
+
+public class Course implements Serializable{
+    private String name;
+    private String courseNumber;
+
+    public Course(String courseNumber, String name) {
+        if (courseNumber.isBlank() && name.isBlank() ){
+            throw new MyException();
+        }
+       this.courseNumber = courseNumber;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCourseNumber() {
+        return courseNumber;
+    }
+
+    public void setCourseNumber(String courseNumber) {
+        this.courseNumber = courseNumber;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("|        %-16s", this.courseNumber) + "|"
+                + String.format("     %-40s", this.name) + "|";
+    }
+
+}
