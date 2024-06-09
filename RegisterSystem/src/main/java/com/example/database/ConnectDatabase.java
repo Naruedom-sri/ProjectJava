d04cd8fe7adaf0ebc30dd1affc85361c9e337ad0
Naruedom-sri <142306666+Naruedom-sri@users.noreply.github.com>
2024-06-09 20:55:04 +0700

    Add files via upload

---
+++ RegisterSystem/src/main/java/com/example/database/ConnectDatabase.java
@@ -0,0 +1,23 @@
+package com.example.database;
+
+import com.example.domain.MyException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectDatabase {
+    private static Connection connection = null;
+    private static String url = "jdbc:mysql://localhost:3306/mydatabase";
+    private static String username = "root";
+    private static String password = "5747";
+
+    public static Connection getConnect(){
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            throw new MyException(e);
+        }
+        return connection;
+    }
+}
